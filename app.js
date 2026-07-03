40c32543753744d9a4a09391501a94426bbb74d5
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-03 15:10:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = 'https://script.google.com/macros/s/AKfycby2g8t5wXxg-01eP4j1-r7kYXvDLM9_j89iNzjFiT3zR8NM0ztYtusBETWp2uFDkkq9-w/exec';
+const API = 'https://script.google.com/macros/s/AKfycbxlYHhH5z00MDCCBV8hDUiqgAGDCmxGocYoyOTq_EYDsa9tfnn9ay2mQvWFdlOYKQQp8w/exec';
 
 // AUTH
 const uStr = sessionStorage.getItem('erp_user');
