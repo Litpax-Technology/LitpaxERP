6cad1d68024508aa81503b123b39c071f8b51d0c
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-13 17:41:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API = 'https://script.google.com/macros/s/AKfycbyH7n2EfZVM6SUMsGJc9ZwEc2z6l5MushTO9vLjJVlOnne6Y1w0Yi7_gFLVBm--CXvkBg/exec';
+const API = 'https://script.google.com/macros/s/AKfycbxuouTj-gMB2Q9YaRY57V3Xh50WesLksOuMFs5ZiqoPODYCHMam6v1JB4qQsngtP4Uayw/exec';
 document.getElementById('pword').addEventListener('keydown', e => {
   if (e.key === 'Enter') doLogin();
 });
