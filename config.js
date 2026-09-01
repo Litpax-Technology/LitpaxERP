63dd6b318a3b88dc69146fc0cfcbf33883e8d399
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-01 13:14:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Single source of truth — GAS deploy URL yahan se change karo, bas
-window.GAS_URL = 'https://script.google.com/a/macros/litpaxtechnology.com/s/AKfycbzZw8Xs5E8DGZXcC7owQVip_egOWi1D6B8fAFzh16r6M72_8dSJ5PgEzeFwESjd00zDRA/exec';
+window.GAS_URL = 'https://script.google.com/a/macros/litpaxtechnology.com/s/AKfycbxu4wdbDoRDlQ1nGTXNjC5vAjB4T1uOSgy--yjWKEIqzpjP9wlKk5S5eGhVGw4vh7faFw/exec';
