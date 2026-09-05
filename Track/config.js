7e95d033adf3a198008631efb742dd82fc2666fc
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-05 12:47:26 +0530

    Update config.js

--- Track/config.js
+++ Track/config.js
@@ -4,7 +4,7 @@
    ============================================================ */
 window.APP_CONFIG = {
   // Client-facing GAS web app ka URL (Anyone access waala) — .../exec
-  API_URL: 'https://script.google.com/macros/s/AKfycbzl087tN_kWOUVa7TQcNZPDxUrccjeKXCj6y2soGZENJ4VzgLmKDybOUituZR3a6goizg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbz_Tw1zWwi9tpm7sj_sao-_Wbf_TBiqTM4tpq6zDL97e4gCDn8ExLjruu8KmOb_ZrCdhg/exec',
 
   COMPANY: 'Litpax Technology',
   SUPPORT_LINE: ''   // optional: help ke liye ek number, khaali chhod sakte ho
