cd416b4177f94d67d302b232b5e25f0bbb6965ce
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-24 09:51:37 +0530

    Update config.js

--- MaterialProd/config.js
+++ MaterialProd/config.js
@@ -8,5 +8,5 @@ window.MP = {
   ERP_URL: 'https://script.google.com/a/macros/litpaxtechnology.com/s/AKfycbzMUP0y2TD8H41kFWi3N6eedaJEju0psUEcJMAUr6cExi8l0RzqHufxCGAkTOmRhlOf_w/exec',
 
   // IMS ka GAS URL — already bhara hua
-  IMS_URL: 'https://script.google.com/macros/s/AKfycbxRyQMM4NJ-pSwWoafXJK1eKS838sWF9HGpJtFEajJogwYDH50BcJLMG6qpnI7JCogi/exec'
+  IMS_URL: 'https://script.google.com/macros/s/AKfycbyN0ZqQ3PUJE2jdq2NbCPPJqJy3lnaqCxSy-ZQJc6yRXQ-AzfX1FAEzv4Vmo2PfwSjO/exec'
 };
